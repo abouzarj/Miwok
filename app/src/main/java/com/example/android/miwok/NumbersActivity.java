8cf4d57b1c2e7696fe403e359103553a88895c0b
Abouzar <abouzarj10@gmail.co>
2019-03-10 06:57:52 +0330

    Use for loop insted of while loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -30,14 +30,11 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        int index = 0;
-
-        while (index < words.size())
-        {
+        for(int index = 0;index < words.size();index++){
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             rootView.addView(wordView);
-            index++;
         }
+
     }
 }
