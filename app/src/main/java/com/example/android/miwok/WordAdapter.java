e23773cdc025296c2ac2cb897c8a5fe4df34e4fe
Abouzar <abouzarj10@gmail.co>
2019-03-24 01:43:23 +0430

    visual polish

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,18 +3,24 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(Activity context, ArrayList<Word> words){
+    //stores the resource id of a color that we want to set as background for textview in list item
+    private int mColorRecourceId;
+    public WordAdapter(Activity context, ArrayList<Word> words , int colorRecourceId){
         super(context,0,words);
+        mColorRecourceId = colorRecourceId;
+
     }
 
     @NonNull
@@ -43,6 +49,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         else {
             imageRecourceId.setVisibility(View.GONE);
         }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorRecourceId);
+        textContainer.setBackgroundColor(color);
+
         return listItemView;
     }
 }
