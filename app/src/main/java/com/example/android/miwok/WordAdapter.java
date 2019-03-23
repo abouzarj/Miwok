f9fd17dd501d676ab5c2234688c398d548cba49c
Abouzar <abouzarj10@gmail.co>
2019-03-23 15:52:28 +0430

    Modify the WordAdapter class add a iamgeview vaiable and add that variable to the object and position

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -32,6 +33,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageRecourceId = (ImageView) listItemView.findViewById(R.id.image);
+        imageRecourceId.setImageResource(currentWord.getImageRecourceId());
 
         return listItemView;
     }
